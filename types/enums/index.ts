dc471cb18f06bd022beb101e3edb311c842e59d6
kojiyamauchi <all.the.small.things@me.com>
2018-11-01 21:18:33 +0900

    added types dir.

---
+++ types/enums/index.ts
@@ -0,0 +1,2 @@
+export enum AddEnumName {}
+// Pending.
---
+++ types/interface/index.ts
@@ -0,0 +1,21 @@
+export interface CityCheckedTypes {
+  buttonPC: string
+  buttonSP: string
+  searchWord: string
+  cityJP: string
+  cityEN: string
+  cityLowerEN: string
+}
+
+export interface TimeAlignmentTypes {
+  time: string
+  meridiem: string
+  militaryTime: string
+}
+
+export interface DataCSV {
+  time: string
+  city: string
+  gender: string
+  population: string
+}
